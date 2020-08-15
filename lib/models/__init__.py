b6bc2b1645d8b24eb7bfa059bc83edb49ff03b5e
Charl P. Botha <cpbotha@vxlabs.com>
2020-08-15 16:16:13 +0200

    Ensure right symbols are available

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -8,9 +8,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import models.pose_resnet
-import models.pose_hrnet
+import lib.models.pose_resnet as pose_resnet
+import lib.models.pose_hrnet as pose_hrnet
